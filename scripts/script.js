c98ee3825f8652356259102107ae951f647e539a
Timur Gafarov <timu.ryan@yandex.com>
2023-02-06 11:54:54 +0300

    edited index scripts

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,15 @@ let popupOpen = document.querySelector(".profile__edit");
 let popupClose = document.querySelector(".popup__close");
 
 let formElement = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__input_name");
-let jobInput = document.querySelector(".popup__input_description");
+let nameInput = document.querySelector(".popup__input_text_name");
+let jobInput = document.querySelector(".popup__input_text_description");
+
+let profileName = document.querySelector(".profile__name-text"); // имя из профиля
+let profileDescription = document.querySelector(".profile__description");
 
 function openPopup() {
-  let name = document.querySelector(".profile__name-text").textContent; // имя из профиля
-  let description = document.querySelector(".profile__description").textContent;
-  nameInput.value = name; //задаем value инпута значения со страницы, чтобы при открытии попапа отображались они
-  jobInput.value = description;
+  nameInput.value = profileName.textContent; //задаем value инпута значения со страницы, чтобы при открытии попапа отображались они
+  jobInput.value = profileDescription.textContent;
   popup.classList.add("popup_opened");
 }
 
@@ -20,14 +21,11 @@ function closePopup() {
 
 function handleFormSubmit(evt) {
   evt.preventDefault(); // отменяет стандартную форму отправки
-  let name = document.querySelector(".profile__name-text");
-  let description = document.querySelector(".profile__description");
-  name.textContent = nameInput.value; // изменяем значения на странице на значения, введенные в импуте
-  description.textContent = jobInput.value;
+  profileName.textContent = nameInput.value; // изменяем значения на странице на значения, введенные в импуте
+  profileDescription.textContent = jobInput.value;
   closePopup();
 }
 
 popupOpen.addEventListener("click", openPopup);
 popupClose.addEventListener("click", closePopup);
-formElement.addEventListener("submit", handleFormSubmit);
 formElement.addEventListener("submit", handleFormSubmit);
